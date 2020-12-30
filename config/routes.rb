b2a5402a2fb1c54486a3d46afe775e5d34a4a116
OTAKE-Soya <oyassun@gmail.com>
2020-12-30 15:05:47 +0000

    favoriteコントローラー、モデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: "users/sessions"}
   
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
   
   root 'homes#top'
   get 'home/about' => 'homes#about'
