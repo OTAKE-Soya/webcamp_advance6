8eb86811c83ad254f8d49f46144129df50a29f88
OTAKE-Soya <oyassun@gmail.com>
2020-12-30 12:05:24 +0000

    エラー修正終わり

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,10 @@
 class BooksController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
-    @book = Book.find(params[:id])
-    @user = @book.user
-    @newbook = Book.new
+    @showbook = Book.find(params[:id])
+    @user = @showbook.user
+    @book = Book.new
   end
 
   def index
@@ -47,6 +48,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 
 end
