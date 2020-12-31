e76275b8d41fe5c1141c4ddfee9cd07bafe2d8ba
OTAKE-Soya <oyassun@gmail.com>
2020-12-31 14:56:09 +0000

    いいね実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,25 @@
 class FavoritesController < ApplicationController
   
   def create
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: book.id)
+    favorite.save
+    if request.path == "books"
+      redirect_to books_path
+    else
+      redirect_to book_path(book.id)
+    end
   end
   
   def destroy
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: book.id)
+    favorite.destroy
+    if request.path == "books"
+      redirect_to books_path
+    else
+      redirect_to book_path(book.id)
+    end
   end
   
   
