bd1c52dfcffb525f15668f062bc8589753b40bfd
OTAKE-Soya <oyassun@gmail.com>
2021-01-03 15:35:37 +0000

    フォロー一覧画面以外はビューできた

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: "users/sessions"}
   
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    member do
+      get :following, :followers 
+    end
+  end
   
   resources :books do
     resource :favorites, only: [:create, :destroy]
